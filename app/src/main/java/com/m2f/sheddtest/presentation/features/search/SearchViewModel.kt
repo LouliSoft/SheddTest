774818a159d9fefa7e377af510942ca45dc753f6
Marc Moreno <ignis.fatue@gmail.com>
2018-04-05 12:43:46 +0200

    added and empty check for the search result

--- app/src/main/java/com/m2f/sheddtest/presentation/features/search/SearchViewModel.kt
+++ app/src/main/java/com/m2f/sheddtest/presentation/features/search/SearchViewModel.kt
@@ -4,7 +4,9 @@ import android.arch.lifecycle.ViewModel
 import android.databinding.ObservableField
 import android.util.Log
 import com.m2f.sheddtest.domain.features.search.SearchImagesInteractor
-import com.m2f.sheddtest.presentation.core.observe
+import com.m2f.sheddtest.domain.features.search.model.TopicImage
+import com.m2f.sheddtest.presentation.core.extensions.invoke
+import com.m2f.sheddtest.presentation.core.extensions.observe
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.plusAssign
 import io.reactivex.rxkotlin.subscribeBy
@@ -22,13 +24,21 @@ class SearchViewModel
 
     val searchText = ObservableField("")
 
+    val isResultEmpty = ObservableField(true)
+
+    val topicList = ObservableField<List<TopicImage>>(listOf())
+
     init {
         compositeDisposable += searchText.observe()
                 .filter { it.isNotBlank() } //we just want non empty text
                 .distinctUntilChanged() //this prevent us to make the same call 2 times in a row
                 .debounce(350L, TimeUnit.MILLISECONDS) //we define a time window of 350 millis to prevent sending every text written.
                 .switchMap { searchImagesInteractor(it) } //we use switchmap here because we are writing alot of text and we want to stop making the previous search if the input changes
-                .subscribeBy(onNext = { it.forEachIndexed { index, topicImage -> Log.d("Search_text_$index", topicImage.image) } })
+                .subscribeBy(onNext = {
+                    it.forEachIndexed { index, topicImage -> Log.d("Search_text_$index", topicImage.image) }
+                    isResultEmpty(it.isEmpty())
+                    topicList(it)
+                })
     }
 
     override fun onCleared() {
@@ -37,3 +47,4 @@ class SearchViewModel
     }
 
 }
+
